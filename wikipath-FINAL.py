9abf7810c70db6bc4faf484d8f852c6b748a4b0a
FAE-Sonata <HeYu1993@gmail.com>
2021-08-05 18:15:22 -0400

    Correct for case where Target is a disambiguation page; comments in .py

--- wikipath-FINAL.py
+++ wikipath-FINAL.py
@@ -15,7 +15,8 @@ while True:
 
 en_wiki_root="https://en.wikipedia.org/wiki/"
 
-
+## run at beginning of search to determine whether either search term 
+## has no article ##
 def has_article(search_term):
     if search_term.strip() == "":
         return False
@@ -25,10 +26,10 @@ def has_article(search_term):
 def find_actual_title_helper(soup_from_page):
     link_titles=soup_from_page.find_all('a', title=True)
     title_text=[elem['title'] for elem in link_titles]
+    ## live articles should have ONE permanent link with revision ID ##
     has_perm=[re.search(re.compile("Permanent link"),x) != None for
              x in title_text]
     idx_perm=np.where(has_perm)
-#    assert(len(idx_perm) == 1)
     if len(idx_perm[0]) != 1:
         return None
     perm_link_suffix = link_titles[idx_perm[0][0]]['href']
@@ -44,7 +45,9 @@ def find_actual_title(s):
     if res == None:
         print("THE FOLLOWING HAD NO PERMANENT REVISION LINK:", s)
     return res
-    
+
+#Given string "search_term", find ALL Article-namespace links on this page
+#Includes links in article body AND in transcluded templates
 def links_on_page(search_term):
     page = requests.get(en_wiki_root + search_term)
     soup_from_page = BeautifulSoup(page.text, 'html.parser')
@@ -67,6 +70,7 @@ def links_on_page(search_term):
     cat_disambig = list(filter(lambda x: re.search(cat_disambig_re, x) != None
                                and re.search(needs_dab_re, x) == None,
                                hrefs))
+    # articles are not added to Disambiguation-labeled categories
     if(len(cat_disambig) > 0):
         return None
     # also exclude the Main Page, which changes daily and will lead to
@@ -80,6 +84,8 @@ def links_on_page(search_term):
     articles = [lk[WIKI_LEN:] for lk in article_links]
     unique_articles = set(articles)
     
+    # remove reflexive links (particularly sectional links, e.g.
+    # "Washington, D.C.#History" )
     this_term_actual_title = find_actual_title_helper(soup_from_page)
     if this_term_actual_title in unique_articles:
         unique_articles.remove(this_term_actual_title)
@@ -87,6 +93,8 @@ def links_on_page(search_term):
                    for s in unique_articles]))
     return res
 
+## find terms that redirect to search_term, e.g.
+## "District of Columbia" to "Washington, D.C."
 def redirects(search_term):
     LEN_IKI = len("iki/")
     links_page_root = en_wiki_root[:-LEN_IKI]
@@ -103,7 +111,7 @@ def redirects(search_term):
     LEN_REDIRECT = len("&redirect=no")
     redirects = [lk[LEN_PREFIX:-LEN_REDIRECT] for lk in redirect_links]
     return(set(redirects))
-
+## condensed version of dictionary: only [key, child] duples returned
 def extract_path_dict(three_in_one_dict):
     return({k: tup[0] for k, tup in three_in_one_dict.items()})
 
@@ -119,41 +127,55 @@ def analytics_print(analytics_dict):
 
 # Reasoning adapted from Wikipedia's article on Breadth First Search
 def bfs(origin_term, target_article, term_search=(False, None), verbose=False):
+    # neither search term should be a disambiguation page; case of origin being
+    # such a page handled below
+    if(links_on_page(target_article) == None):
+        print("Target term is a disambiguation page")
+        return None
     TARGET_REDIRECTS = redirects(target_article)
     future_vertices = deque()
     visited = set(); count_wikilinks = 0
     three_in_one_dict = dict()
-    # values: (parent article, distance from root, number of links on parent article, linked as)
-    link_dict = dict()  # added for perf
+    ## values in three_in_one_dict: (parent article, distance from root, number
+    ## of links on parent article)
+    ### miscellaneous statistics ###
+    link_dict = dict() # added for perf
     noentry_count = 0; disambig_count = 0; redirect_count = 0
     current_streak = 0; max_streak = 0
-    # current_streak refers to red links, redirects, or disambiguations
+    ## current_streak refers to red links (no article), redirects, or
+    ## disambiguations
+    
     root = origin_term
-    # key stores the parent node
-    three_in_one_dict[root] = (None, 0, None) #, None)
+    three_in_one_dict[root] = (None, 0, None)
     analytics_dict = dict()
     current_level = -1; max_dist = 0
     future_vertices.appendleft(root)
+    ### for progress updates during particularly large queries ###
     levels = [1E4, 2.5E4, 5E4, 7.5E4, 1E5, 2E5, 2.5E5, 3E5, 4E5, 5E5, 7.5E5,
               1E6, 1.5E6, 2E6, 2.5E6]
     levels.sort()
-#    pd.DataFrame({'levels': levels, 'visited'})
+    ### tracking based on the levels above ###
     attained_levels = set()
     while len(future_vertices) > 0:
+        # remove next article off Queue (future_vertices) to examine #
         subtree_root = future_vertices.pop()
         actual_title = find_actual_title(subtree_root)
         if(actual_title == None):
             # linked from a page, but no article on EN Wikipedia yet for this term
             noentry_count += 1; current_streak += 1
+            # not useful for any tracking dictionary #
             three_in_one_dict.pop(subtree_root)
             if verbose:
                 print("Dead end: " + subtree_root)
+            # next entry in Queue (future_vertices); nothing to be done
             continue
+        ## encountered redirect off the Queue (future_vertices)
         if(actual_title in visited):
             redirect_count += 1; current_streak += 1
 #            if verbose:
 #                print("Encountered redirect or term was previously visited:",
 #                      subtree_root)
+            # next entry in Queue (future_vertices); nothing to be done
             continue
         if verbose:
             num_future = len(future_vertices)
@@ -167,6 +189,7 @@ def bfs(origin_term, target_article, term_search=(False, None), verbose=False):
                       str(disambig_count), " disambiguation pages, ",
                       str(redirect_count), " redirects==",
                       sep="")
+            ### referring to certain milestones for |path_dict|, e.g. 100K, 1M
             if(current_level + 1 < len(levels)):
                 next_level = levels[current_level + 1]
                 if not(next_level in attained_levels) and len(
@@ -187,19 +210,19 @@ def bfs(origin_term, target_article, term_search=(False, None), verbose=False):
                     current_level += 1
        
         current_dist = three_in_one_dict[subtree_root][1]
+        ## done exploring all vertices (articles) at current level ##
         if current_dist > max_dist:
-            ### POTENTIAL SORTING FOR LV 2 and beyond ###
             if max_dist > 0:
+                ### analytics: sorting visited articles at previous level by
+                ### number of links on page
                 unique_visited = visited.intersection(three_in_one_dict.keys())
                 list_unique_visited = list(unique_visited)
                 list_unique_visited.sort()
+                # zip together dictionary {article: links}, reverse sort
                 terms_prev_lv = list(filter(lambda k:
                     three_in_one_dict[k][1]==max_dist, unique_visited))
-#                print(terms_prev_lv)
                 num_links = [link_dict[k] for k in terms_prev_lv]
                 link_tuples = list(zip(terms_prev_lv, num_links))
-#                print(running_max_noncompliant(num_links))
-#                print(running_min_noncompliant(num_links))
                 link_tuples.sort(key=lambda tup: tup[1], reverse=True)
                 if len(link_tuples) > 10:
                     print(link_tuples[:10])
@@ -209,6 +232,7 @@ def bfs(origin_term, target_article, term_search=(False, None), verbose=False):
             if verbose:
                 num_future = len(future_vertices)
                 analytics_print(analytics_dict)
+                print("==========")
                 print("Reached depth ", current_dist, " with term ",
                       subtree_root, ", having visited ", str(count_wikilinks),
                       " articles and with ", str(num_future),
@@ -216,95 +240,95 @@ def bfs(origin_term, target_article, term_search=(False, None), verbose=False):
                       str(num_future / count_wikilinks), ")",
                       sep="")
         if subtree_root == target_article: # can only happen with redirects
-            if verbose:
-                print("The search terms redirect to the same article")
             return construct_path(subtree_root, extract_path_dict(
                     three_in_one_dict))
             
-        links = links_on_page(subtree_root)
-        if(links == None): # Skipping: Disambiguation page, not an article
+        links_this_page = links_on_page(subtree_root)
+        if(links_this_page == None): # Skipping: Disambiguation page, not an article
             disambig_count += 1; current_streak += 1
             if verbose:
                 print("Disambiguation page removed: " + subtree_root)
             continue
-        
+        ### update max streak of red links (no article), redirects, or
+        ### disambiguation pages, reset current streak to 0 since this term in
+        ### Queue was not skipped
         if current_streak > max_streak:
             max_streak = current_streak
             if verbose:
                 print("Maximum streak extended to " + str(current_streak))
         current_streak = 0
 
-        link_dict[subtree_root] = len(links)
+        link_dict[subtree_root] = len(links_this_page)
         if actual_title != subtree_root:
-            link_dict[actual_title] = len(links)
-        link_names = set(links.keys())
+            link_dict[actual_title] = len(links_this_page)
+        link_names = set(links_this_page.keys())
         # there is a link on this page to the target
         if target_article in link_names:
             three_in_one_dict[target_article] = (subtree_root, current_dist+1,
-                             len(links))
+                             len(links_this_page))
             if verbose:
                 print("1st degree connection:", target_article, "-->",
-                      links[target_article])
+                      links_this_page[target_article])
                 if current_dist > 0:
                     print("Visited", str(count_wikilinks), "articles;",
                           "Added", str(len(three_in_one_dict)),
                           "entries to path dictionary")
             return construct_path(target_article, extract_path_dict(
                     three_in_one_dict))
-        # O(1)
+
+        ## return if any links on this page are REDIRECTS to target article ##
         redirects_in_links = link_names.intersection(TARGET_REDIRECTS)
         if len(redirects_in_links) > 0:
             print("Linked to redirect:")
             random_redirect = list(redirects_in_links)[0]
             three_in_one_dict[random_redirect] = (subtree_root,
-                             current_dist+1, len(links))
+                             current_dist+1, len(links_this_page))
             if current_dist > 0:
                 print("Visited", str(count_wikilinks), "articles;", "Added",
                       str(len(three_in_one_dict)), "entries to path",
                       "dictionary")
             return construct_path(random_redirect,
                                   extract_path_dict(three_in_one_dict))
-        link_children = set(links.keys())
+        # CLEAN LINKS #
+        link_children = set(links_this_page.keys())
         # no self links
-        if term_search[0] and (re.match(term_search[1], subtree_root) != None
-                      or re.match(term_search[1], actual_title) != None):
-            print("^^Removing self reference^^")
         link_children.discard(subtree_root)
         # remove articles that have been previously visited
-        if term_search[0] and (re.match(term_search[1], subtree_root) != None
-                      or re.match(term_search[1], actual_title) != None):
-            print("^^Removing previous visits^^")
         link_children.difference_update(visited)
         # remove articles that have already been marked for visitation
-        if term_search[0] and (re.match(term_search[1], subtree_root) != None
-                      or re.match(term_search[1], actual_title) != None):
-            print("^^Removing future visits^^")
         link_children.difference_update(set(future_vertices))
-        if term_search[0] and (re.match(term_search[1], subtree_root) != None
-                      or re.match(term_search[1], actual_title) != None):
-            print("^^Pruned. Adding children to dictionary and to-visit set.^^")
         for child in link_children:
+            ## CONSTRUCT DICTIONARY with entry of valid child article linked:
+            ## 1. parent linked to (i.e. this page that was last popped off Queue)
+            ## 2. distance from 1st article, and
+            ## 3. number of links on page
             three_in_one_dict[child] = (subtree_root,
-                             current_dist+1, len(links)) #, child)
+                             current_dist+1, len(links_this_page))
             future_vertices.appendleft(child)
         num_new_links = len(link_children)
-        num_article_links = len(links)
-        if(re.match(term_search[1], subtree_root) != None
-           or num_new_links >= 1000 or num_article_links >= 2000):
-            print("^Processing " + subtree_root + " for link_children^")
-            if num_new_links == 0:
-                print("^^No new entries were added to dictionary,",
-                      " though ", num_article_links, " unique links were",
-                      " present.^^", sep="")
-            else:
-                print("^^Added ", num_new_links, " entries to dictionary,",
-                      " with ", num_article_links, " unique links on the page",
-                      " (", 100 * num_new_links / num_article_links,
-                      "%).^^", sep="")
+        num_article_links = len(links_this_page)
+        if(term_search[0] and term_search[1] != None):
+            if(re.match(term_search[1], subtree_root) != None
+               or num_new_links >= 1000 or num_article_links >= 2000):
+                print("^Processing " + subtree_root + " for link_children^")
+                if num_new_links == 0:
+                    print("^^No new entries were added to dictionary,",
+                          " though ", num_article_links, " unique links were",
+                          " present.^^", sep="")
+                else:
+                    print("^^Added ", num_new_links, " entries to dictionary,",
+                          " with ", num_article_links, " unique links on the page",
+                          " (", 100 * num_new_links / num_article_links,
+                          "%).^^", sep="")
+        ### NOT equal to cumulative |future_vertices|, since it excludes
+        ### disambiguation pages and redirects
         count_wikilinks += 1
         visited.add(subtree_root)
         if actual_title != subtree_root:
             visited.add(actual_title)
+        ### number of fully processed articles: (|future queue|, |path dict|)
+        ### the ratio |future queue| / count_wikilinks is an approximation of
+        ### the mean number of links newly added to the "future queue"
         analytics_dict[count_wikilinks] = (len(future_vertices),
                       len(three_in_one_dict))
 
@@ -322,10 +346,10 @@ def construct_path(destination, path_dict):
 def process(s1, s2):
     valid1 = has_article(s1); valid2 = has_article(s2)
     if not(valid1):
-        print("No such path exists. English Wikipedia does not have the" + 
+        print("No such path exists. English Wikipedia does not have the " + 
               "following term: ", s1)
     if not(valid2):
-        print("No such path exists. English Wikipedia does not have the" + 
+        print("No such path exists. English Wikipedia does not have the " + 
               "following term: ", s2)
     if valid1 and valid2:
         article1 = find_actual_title(s1)
@@ -334,17 +358,12 @@ def process(s1, s2):
             print("\'" + s1 + "\'" + " and " + "\'" + s2 + "\'" +
                   " redirect to " + article1)
         else:
-            print("PROCESSING", s1, "and", s2)
+            print("PROCESSING '%s' and '%s'" % (s1, s2))
             resultant_path = bfs(article1,
                                  article2,
-                                 term_search=(False,
-#                                              re.compile("Alask|Isl|Arct",
-#                                              re.compile("Shanghai|Putuo",
-#                                              re.compile("^(Sich|Chengd)",
-                                              re.compile("Chiba|Narita|Tokyo|Airport",
-#                                              re.compile("(Maca(o|u))|(^Hong)|Guangdong",
-                                                         re.I)),
-                                              verbose=True)
+                                 # term_search=(False, None),
+                                 # can use re.compile("...")
+                                 verbose=True)
             print(resultant_path)
 
 if is_csv_input:
